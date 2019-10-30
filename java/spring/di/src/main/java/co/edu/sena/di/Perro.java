0207929a565b508e3a57821f6fa14aeedf7295b8
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-10-30 08:17:17 -0500

    chore: ejemplo de inyección de dependencia

---
+++ java/spring/di/src/main/java/co/edu/sena/di/Perro.java
@@ -0,0 +1,9 @@
+package co.edu.sena.di;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class Perro {
+}
