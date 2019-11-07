de2aca8800296f3b57073feb5af456b4b94dd72a
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-11-07 10:42:44 -0500

    chore: configuración para tomcat

--- java/spring/mapeosurrogate/src/main/java/co/edu/sena/horariossurogate/MapeoSurrogateApplication.java
+++ java/spring/mapeosurrogate/src/main/java/co/edu/sena/horariossurogate/MapeoSurrogateApplication.java
@@ -2,17 +2,29 @@ package co.edu.sena.horariossurogate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class MapeoSurrogateApplication {
+@RestController
+public class MapeoSurrogateApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MapeoSurrogateApplication.class);
+    }
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MapeoSurrogateApplication.class, args);
-
-
 	}
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Funciona el servidor con REST";
+    }
 
 }
--- java/spring/mapeosurrogate/src/main/java/co/edu/sena/horariossurogate/web/rest/TipoDocumentoResource.java
+++ java/spring/mapeosurrogate/src/main/java/co/edu/sena/horariossurogate/web/rest/TipoDocumentoResource.java
@@ -3,7 +3,6 @@ package co.edu.sena.horariossurogate.web.rest;
 
 import co.edu.sena.horariossurogate.domain.TipoDocumento;
 import co.edu.sena.horariossurogate.repository.TipoDocumentoRepository;
-import com.sun.org.apache.bcel.internal.generic.ATHROW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
